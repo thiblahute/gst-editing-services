d2c20ccf4679777e1201388a287cd5572b1b499c
Thibault Saunier <tsaunier@gnome.org>
2015-03-13 12:02:30 +0000

    ges-launch: Prefix clip, transition and effect instruction with a +
    Slightly changing the CLI so that we have indicators of the timeline
    commands adding new objects.

--- tools/ges-launch.c
+++ tools/ges-launch.c
@@ -642,7 +642,7 @@ _print_all_commands (void)
 {
   /* Yeah I know very fancy */
   g_print ("Available ges-launch-1.0 commands:\n\n");
-  g_print ("  %-8s %-10s %-10s\n\n", "clip", "effect", "set-");
+  g_print ("  %-9s %-11s %-10s\n\n", "+clip", "+effect", "set-");
   g_print ("See ges-launch-1.0 help <command> or ges-launch-1.0 help <guide> "
       "to read about a specific command or a given guide\n");
 }
